a1ce500bd6691294146f674bb33613655d801e5b
devProgOussou <56416302+devProgOussou@users.noreply.github.com>
2020-11-28 11:06:38 +0000

    Update Company.php

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -11,7 +11,7 @@ class Company extends Model
     use HasFactory;
 
     protected $fillable = [
-        'lastName', 'firstName', 'companyName', 'address', 'phone', 'user_id'
+        'companyName', 'address', 'phone', 'user_id'
     ];
 
     public function user()
