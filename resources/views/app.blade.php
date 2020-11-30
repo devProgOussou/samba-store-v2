ee65318eeb5519539e429b6a773f36c66d213e46
Ousmane Fall <56416302+devProgOussou@users.noreply.github.com>
2020-11-30 16:29:35 +0000

    Add files via upload

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,33 +1,18 @@
-{{-- <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
-  </head>
-  <body>
-    @inertia
-  </body>
-</html> --}}
-
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    {{-- <link href="{{ asset('/css/app.css') }}" rel="stylesheet" /> --}}
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
-    {{-- <script src="{{ asset('/js/app.js') }}" defer></script> --}}
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #008080">
@@ -53,6 +38,7 @@
                 </a>
                 @endif
 
+            @if(Auth::check())
             <ul class="dropdown-menu dropdown-menu-right" role="menu">
                 @if(Auth::check() && Auth::user()->isActive == true)
                 <a class="dropdown-item" href="{{ url('/home') }}">
@@ -65,7 +51,7 @@
                 <a class="dropdown-item" href="{{ url('/message') }}">
                     Messagerie
                 </a>
-                @if(Auth::check() && Auth::user()->isRegister == true)
+                @if(Auth::check())
                 <a class="dropdown-item" href="{{ url('/updateUserProfile/'.Auth::user()->id) }}">
                     Modifier profil
                 </a>
@@ -83,6 +69,7 @@
                     Deconnexion
                 </a>
             </ul>
+            @endif
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                 @csrf
             </form>
